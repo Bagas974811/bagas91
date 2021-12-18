02d268ffe32da8056fc8546617859c567d427cc8
Bagas974811 <96328849+Bagas974811@users.noreply.github.com>
2021-12-18 13:39:55 +0700

    Create cara_bagus_nambang_VRSC.sh

---
+++ cara_bagus_nambang_VRSC.sh
@@ -0,0 +1,5 @@
+#nambang Veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://verushash.sea.mine.zpool.ca:6143#xnsub -u Xhf3vVC9Wu2NSdYRH1RwJjiZcbx7qCgrrj -p hellmin,c=DASH --cpu 4
